ab9b733adcabe1ed6f6405538d4f752bd2c960a8
Adham Abbas <88adhamtiger@gmail.com>
2024-07-02 19:25:45 +0000

    v12

--- services/BuyService.js
+++ services/BuyService.js
@@ -1,3 +1,38 @@
+const asyncHandler = require('express-async-handler');
+const ApiFeatures = require('../utils/apiFeatures');
+const XLSX = require('xlsx');
+const factory = require('./handlersFactory');
+const Buy = require('../models/BuyModel');
+
+
+
+
+
+
+// @desc    Get list of Buy
+// @route   GET /api/v1/Buys
+// @access  Public
+exports.getBuys = factory.getAll(Buy,'Buy');
+
+// @desc    Get specific Buy by id
+// @route   GET /api/v1/Buys/:id
+// @access  Public
+exports.getBuy = factory.getOne(Buy,'supplayr');
+
+// @desc    Create Buy
+// @route   POST  /api/v1/Buys
+// @access  Private
+exports.createBuy = factory.createOne(Buy);
+// @desc    Update specific Buy
+// @route   PUT /api/v1/Buys/:id
+// @access  Private
+exports.updateBuy = factory.updateOne(Buy);
+
+// @desc    Delete specific Buy
+// @route   DELETE /api/v1/Buys/:id
+// @access  Private
+exports.deleteBuy = factory.deleteOne(Buy);
+
 exports.printExcel = (Buy, modelName = 'Supplayr') => asyncHandler(async (req, res) => {
   try {
       let filter = {};
