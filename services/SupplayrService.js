ce4434584d004f7cc65b50f65381af55c7ff780d
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2024-07-16 17:31:01 +0300

    Update SupplayrService.js

--- services/SupplayrService.js
+++ services/SupplayrService.js
@@ -1,6 +1,10 @@
-
+const asyncHandler = require('express-async-handler');
+const ApiError = require('../utils/apiError');
 const factory = require('./handlersFactory');
-const Supplayr = require('../models/SupplayrModel')
+const Supplayr = require('../models/SupplayrModel');
+const Buy =require('../models/BuyModel');
+const Buy_bell =require('../models/Buy_bellModel');
+const Supplayr_tax =require('../models/Tax_supplayrModel')
 
 
 
@@ -29,3 +33,25 @@ exports.updateSupplayr = factory.updateOne(Supplayr);
 // @route   DELETE /api/v1/Supplayr/:id
 // @access  Private
 exports.deleteSupplayr = factory.deleteOne(Supplayr);
+
+exports.getSupplayrDetails = asyncHandler(async (req, res, next) => {
+    const { supplayrId } = req.params;
+  
+    // Get all sales for the supplayr
+    const bell = await Buy_bell.find({ supplayr: supplayrId })
+      .populate({ path: 'supplayr', select: 'supplayr_name' });
+  
+    // Get all purchases for the supplayr
+    const buys = await Buy.find({ supplayr: supplayrId })
+      .populate({ path: 'supplayr', select: 'supplayr_name' });
+  
+    const tax = await Supplayr_tax.find({ supplayr: supplayrId })
+      .populate({ path: 'supplayr', select: 'supplayr_name' });
+  
+    if (!bell && !buys && !tax ) {
+      return next(new ApiError(`No transactions found for client with ID: ${supplayrId}`, 404));
+    }
+  
+    res.status(200).json({ buys , bell ,tax });
+  });
+  
