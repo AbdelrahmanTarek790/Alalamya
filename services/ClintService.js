062666d66017c78eb20e2c279bf63ab56c7556a3
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2024-07-16 17:30:28 +0300

    Update ClintService.js

--- services/ClintService.js
+++ services/ClintService.js
@@ -1,7 +1,11 @@
-
+const asyncHandler = require('express-async-handler');
+const ApiError = require('../utils/apiError');
 const factory = require('./handlersFactory');
 const Clint = require('../models/ClintModel');
-
+const Sell =require('../models/sellModel');
+const Sell_bell = require('../models/Sell_bellModel');
+const clint_tax = require('../models/Tax_clintModel');
+const check_back = require('../models/ReturnCheckModel');
 
 
 
@@ -29,3 +33,29 @@ exports.updateClint = factory.updateOne(Clint);
 // @route   DELETE /api/v1/Clint/:id
 // @access  Private
 exports.deleteClint = factory.deleteOne(Clint);
+
+// services/clintService.js
+
+exports.getClientDetails = asyncHandler(async (req, res, next) => {
+  const { clientId } = req.params;
+
+  // Get all sales for the client
+  const bell = await Sell_bell.find({ clint: clientId })
+    .populate({ path: 'clint', select: 'clint_name' });
+
+  // Get all purchases for the client
+  const sela = await Sell.find({ clint: clientId })
+    .populate({ path: 'clint', select: 'clint_name' });
+
+  const tax = await clint_tax.find({ clint: clientId })
+    .populate({ path: 'clint', select: 'clint_name' });
+  
+  const chBack = await check_back.find({ clint: clientId })
+    .populate({ path: 'clint', select: 'clint_name' });
+
+  if (!bell && !sela && !tax  && !chBack) {
+    return next(new ApiError(`No transactions found for client with ID: ${clientId}`, 404));
+  }
+
+  res.status(200).json({  sela , bell , chBack ,tax });
+});
