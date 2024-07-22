f0ae02f282bf8fab180516f2cba02dcafa5a4a26
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2024-07-22 23:00:22 +0300

    Update Buy_bellModel.js

--- models/Buy_bellModel.js
+++ models/Buy_bellModel.js
@@ -18,6 +18,11 @@ const Buy_bellSchema = new mongoose.Schema(
       required: true,
       default: 0,
     },
+    Notes: {
+      type:String,
+      trim: true,
+      default:'',
+    },
     payment_method: {
       type: String,
       enum: ['cash', 'check'],
