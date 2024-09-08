dbba2b9d95db177c7ef554650786bd4d053e0260
Adham Abbas <88adhamtiger@gmail.com>
2024-09-08 22:07:47 +0000

    v55

--- routes/ReportRoute.js
+++ routes/ReportRoute.js
@@ -3,22 +3,22 @@ const authService = require('../services/authService');
 const { generateReport } = require('../services/Report');
 const router = express.Router();
 
-router.get('/', authService.protect,authService.allowedTo('admin'),
-    async (req, res) => {
-  const { startDate, endDate } = req.query;
-
-  try {
-    const report = await generateReport(startDate, endDate);
-    res.status(200).json({
-      status: 'success',
-      data: report
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: err.message
-    });
+router.get('/', authService.protect, authService.allowedTo('admin'),
+  async (req, res) => {
+    try {
+      // استدعاء دالة generateReport بدون معلمات
+      const report = await generateReport();
+      res.status(200).json({
+        status: 'success',
+        data: report
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'error',
+        message: err.message
+      });
+    }
   }
-});
+);
 
 module.exports = router;
