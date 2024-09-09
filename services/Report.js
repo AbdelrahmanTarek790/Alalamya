442ad25e9a7c91355409ec0627ff691f39823a99
Adham Abbas <88adhamtiger@gmail.com>
2024-09-09 06:37:14 +0000

    v55

--- services/Report.js
+++ services/Report.js
@@ -6,12 +6,7 @@ const Supplier = require('../models/SupplayrModel');
 const Product = require('../models/ProductModel');
 
 exports.generateReport = async () => {
-  // الحصول على الشهر والسنة الحاليين
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() + 1; // الأشهر في JavaScript تبدأ من 0
-  const currentYear = currentDate.getFullYear();
-
-  // حساب إجمالي المبيعات
+  
   const totalSales = await Sell.aggregate([
     {
       $group: {
@@ -22,55 +17,7 @@ exports.generateReport = async () => {
     }
   ]);
 
-  // حساب المالية للمبيعات في الشهر الحالي باستخدام entry_date
-  const salesByMonth = await Sell.aggregate([
-    {
-      $project: {
-        month: { $month: '$entry_date' },
-        year: { $year: '$entry_date' },
-        price_allQuantity: 1,
-        o_wieght: 1
-      }
-    },
-    {
-      $match: {
-        month: currentMonth,
-        year: currentYear
-      }
-    },
-    {
-      $group: {
-        _id: { month: currentMonth, year: currentYear },
-        totalAmount: { $sum: '$price_allQuantity' },
-        totalWeight: { $sum: '$o_wieght' }
-      }
-    }
-  ]);
-
-  // حساب المالية للمشتريات في الشهر الحالي باستخدام entry_date
-  const purchasesByMonth = await Buy.aggregate([
-    {
-      $project: {
-        month: { $month: '$entry_date' },
-        year: { $year: '$entry_date' },
-        price_all: 1
-      }
-    },
-    {
-      $match: {
-        month: currentMonth,
-        year: currentYear
-      }
-    },
-    {
-      $group: {
-        _id: { month: currentMonth, year: currentYear },
-        totalAmount: { $sum: '$price_all' }
-      }
-    }
-  ]);
-
-  // حساب الأموال المدفوعة من العملاء باستخدام Sell و Sell_bell
+  
   const totalPaidByClientsFromSell = await Sell.aggregate([
     {
       $group: {
@@ -92,7 +39,7 @@ exports.generateReport = async () => {
   const totalPaidByClients = (totalPaidByClientsFromSell.length > 0 ? totalPaidByClientsFromSell[0].totalPaid : 0) +
                              (totalPaidByClientsFromSellBell.length > 0 ? totalPaidByClientsFromSellBell[0].totalPaid : 0);
 
-  // حساب الأموال المتبقية على العملاء
+  
   const totalDueFromClients = await Clint.aggregate([
     {
       $group: {
@@ -102,7 +49,7 @@ exports.generateReport = async () => {
     }
   ]);
 
-  // حساب الأموال المدفوعة للموردين باستخدام Buy فقط
+  
   const totalPaidToSuppliersFromBuy = await Buy.aggregate([
     {
       $group: {
@@ -114,7 +61,6 @@ exports.generateReport = async () => {
 
   const totalPaidToSuppliers = totalPaidToSuppliersFromBuy.length > 0 ? totalPaidToSuppliersFromBuy[0].totalPaid : 0;
 
-  // حساب الأموال المتبقية على الموردين
   const totalDueToSuppliers = await Supplier.aggregate([
     {
       $group: {
@@ -124,7 +70,7 @@ exports.generateReport = async () => {
     }
   ]);
 
-  // حساب إجمالي المشتريات من الموردين باستخدام Buy فقط
+  
   const totalPurchasesFromBuy = await Buy.aggregate([
     {
       $group: {
@@ -136,10 +82,10 @@ exports.generateReport = async () => {
 
   const totalPurchases = totalPurchasesFromBuy.length > 0 ? totalPurchasesFromBuy[0].totalAmount : 0;
 
-  // حساب إجمالي الربح (إجمالي المبيعات - إجمالي المشتريات)
+  
   const totalProfit = totalSales.length > 0 ? totalSales[0].totalAmount - totalPurchases : 0;
 
-  // حساب إجمالي wight_money لجميع المنتجات
+  
   const totalWightMoney = await Product.aggregate([
     {
       $group: {
@@ -148,6 +94,52 @@ exports.generateReport = async () => {
       }
     }
   ]);
+
+  exports.generateReport = async () => {
+   
+    const currentMonth = new Date().getMonth() + 1; 
+
+    
+    const monthlySales = await Sell.aggregate([
+        {
+            $match: {
+                entry_date: {
+                    $gte: new Date().setMonth(new Date().getMonth() - 1, 1), // Start of current month
+                    $lt: new Date().setMonth(new Date().getMonth() + 1, 1)  // Start of next month
+                }
+            }
+        },
+        {
+            $group: {
+                _id: { $month: '$entry_date' },
+                totalAmount: { $sum: '$price_allQuantity' },
+                totalWeight: { $sum: '$o_wieght' }
+            }
+        }
+    ]);
+
+    
+    const monthlyPurchases = await Buy.aggregate([
+      {
+        $match: {
+            Entry_date: {
+                $gte: new Date().setMonth(new Date().getMonth() - 1, 1), // Start of current month
+                $lt: new Date().setMonth(new Date().getMonth() + 1, 1)  // Start of next month
+            }
+        }
+    },
+    {
+        $group: {
+            _id: { $month: '$Entry_date' },
+            totalAmount: { $sum: '$price_all' },
+            totalWeight: { $sum: '$E_wieght' }
+        }
+    }
+    ]);
+
+  
+   
+};
 
   return {
     totalSales: totalSales.length > 0 ? totalSales[0].totalAmount : 0,
@@ -158,8 +150,8 @@ exports.generateReport = async () => {
     totalDueToSuppliers: totalDueToSuppliers.length > 0 ? totalDueToSuppliers[0].totalDue : 0,
     totalPurchases,
     totalProfit,
-    totalWightMoney: totalWightMoney.length > 0 ? totalWightMoney[0].totalWightMoney : 0,
-    salesByMonth,  // المالية للمبيعات في الشهر الحالي
-    purchasesByMonth  // المالية للمشتريات في الشهر الحالي
+    totalWightMoney: totalWightMoney.length > 0 ? totalWightMoney[0].totalWightMoney : 0 ,
+    monthlySales,
+    monthlyPurchases 
   };
 };
