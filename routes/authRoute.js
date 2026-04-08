b8e8303da9ff7a7869f77ad28ffb9aba1ac796b4
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2026-04-08 11:46:38 +0200

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -12,7 +12,7 @@ const {
 
 const router = express.Router();
 
-router.post('/signup', signupValidator, signup);
+router.post('/signup',signup);
 router.post('/login', loginValidator, login);
 router.get('/me',getUserData);
 module.exports = router;
